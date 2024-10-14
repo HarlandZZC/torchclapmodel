ef0919e3a4499e5ed37fd99a08799337c172794e
HalrandZZC <harlandzzc@gmail.com>
2024-10-14 13:43:50 +0800

    2024-1014-1343

--- hubconf.py
+++ hubconf.py
@@ -7,13 +7,13 @@ import transformers
 
 def clapmodel(weights_name, device):
     model_urls = {
-    'music_speech_audioset_epoch_15_esc_89.98': 'https://huggingface.co/lukewys/laion_clap/resolve/main/'
+    'music_speech_audioset_epoch_15_esc_89.98.pt': 'https://huggingface.co/lukewys/laion_clap/resolve/main/'
             'music_speech_audioset_epoch_15_esc_89.98.pt',
-    'music_audioset_epoch_15_esc_90.14': 'https://huggingface.co/lukewys/laion_clap/resolve/main/'
+    'music_audioset_epoch_15_esc_90.14.pt': 'https://huggingface.co/lukewys/laion_clap/resolve/main/'
            'music_audioset_epoch_15_esc_90.14.pt',
-    'music_speech_epoch_15_esc_89.25': 'https://huggingface.co/lukewys/laion_clap/resolve/main/'
+    'music_speech_epoch_15_esc_89.25.pt': 'https://huggingface.co/lukewys/laion_clap/resolve/main/'
             'music_speech_epoch_15_esc_89.25.pt',
-    '630k-audioset-fusion-best': 'https://huggingface.co/lukewys/laion_clap/resolve/main/'
+    '630k-audioset-fusion-best.pt': 'https://huggingface.co/lukewys/laion_clap/resolve/main/'
             '630k-audioset-fusion-best.pt'}
     
     def load_state_dict_from_url(url: str, map_location="cpu", skip_params=True, progress=True):
@@ -29,18 +29,18 @@ def clapmodel(weights_name, device):
             del state_dict["text_branch.embeddings.position_ids"]
         return state_dict
     
-    if weights_name == "music_speech_audioset_epoch_15_esc_89.98":
+    if weights_name == "music_speech_audioset_epoch_15_esc_89.98.pt":
         model = laion_clap.CLAP_Module(enable_fusion=False, amodel='HTSAT-base',  device=device)
-        pkg = load_state_dict_from_url(model_urls['music_speech_audioset_epoch_15_esc_89.98'], progress=True)
-    elif weights_name == "music_audioset_epoch_15_esc_90.14":
+        pkg = load_state_dict_from_url(model_urls['music_speech_audioset_epoch_15_esc_89.98.pt'], progress=True)
+    elif weights_name == "music_audioset_epoch_15_esc_90.14.pt":
         model = laion_clap.CLAP_Module(enable_fusion=False, amodel='HTSAT-base',  device=device)
-        pkg = load_state_dict_from_url(model_urls['music_audioset_epoch_15_esc_90.14'], progress=True)
-    elif weights_name == "music_speech_epoch_15_esc_89.25":
+        pkg = load_state_dict_from_url(model_urls['music_audioset_epoch_15_esc_90.14.pt'], progress=True)
+    elif weights_name == "music_speech_epoch_15_esc_89.25.pt":
         model = laion_clap.CLAP_Module(enable_fusion=False, amodel='HTSAT-base',  device=device)
-        pkg = load_state_dict_from_url(model_urls['music_speech_epoch_15_esc_89.25'], progress=True)
-    elif weights_name == "630k-audioset-fusion-best":
+        pkg = load_state_dict_from_url(model_urls['music_speech_epoch_15_esc_89.25.pt'], progress=True)
+    elif weights_name == "630k-audioset-fusion-best.pt":
         model = laion_clap.CLAP_Module(enable_fusion=True,  device=device)
-        pkg = load_state_dict_from_url(model_urls['630k-audioset-fusion-best'], progress=True)
+        pkg = load_state_dict_from_url(model_urls['630k-audioset-fusion-best.pt'], progress=True)
     else:
         raise ValueError('clap_model not implemented')
 
